b5d7b5d0b89e15326986ab9005c6426d33ecb58b
Ram Persaud <rogue07@users.noreply.github.com>
2023-01-11 23:25:51 -0500

    Add files via upload
    Updated logging so it shows the user's first and last name when they successfully use a registered fob/card.

--- scanIn.py
+++ scanIn.py
@@ -9,6 +9,13 @@ from datetime import datetime
 from adafruit_pn532.spi import PN532_SPI
 
 
+# log to accessc.log
+logfile = "/home/accessc/Documents/accessc.log"
+#logging.basicConfig(filename="accessc.log", lvell=logging.INFO)
+logging.basicConfig(filename="/home/accessc/Documents/accessc.log", format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+
+
 now = datetime.now()
 today = now.strftime("%d/%m/%Y %H:%M")
    
@@ -35,14 +42,27 @@ while True:
         continue
     print("Found card with UID:", [hex(i) for i in uid])
     usercard = [hex(i) for i in uid]
-    
+   
+    logging.info('Card was presented')
+    time.sleep(5)
+
     mycursor.execute(f'SELECT EXISTS(SELECT * FROM accessc WHERE card = "{usercard}") as OUTPUT')
     myresult = mycursor.fetchone()
     print(myresult)
+    mycursor.execute(f'SELECT last FROM accessc WHERE card = "{usercard}"')
+    lname = mycursor.fetchone()
+    mycursor.execute(f'SELECT first FROM accessc WHERE card = "{usercard}"')
+    fname = mycursor.fetchone()
+    print(fname, lname)
+
     time.sleep(5)
-    if myresult == (1,):
-        print("Card/User accepted")
-        time.sleep(5)
+    
+
+    if myresult != (1,):
+        print("Access failed")
+        logging.info('Failed access')
+        time.sleep(2)
     else:
-        print("Failed access")
-        time.sleep(5)
+        print("Access successful")
+        logging.info(f'{fname} {lname} successful access')
+        time.sleep(2)
