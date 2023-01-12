2688c6149a18f606d5b1199a967ca9bd78a58851
Ram Persaud <rogue07@users.noreply.github.com>
2023-01-11 19:57:52 -0500

    Add files via upload
    Fixed logging so it logs to the correct directory and log file.

--- emergencylock.py
+++ emergencylock.py
@@ -4,7 +4,7 @@ import logging
 import subprocess
 
 
-logfile = "/home/Documents/accessc.log"
+logfile = "/home/accessc/Documents/accessc.log"
 #logging.basicConfig(filename="accessc.log", level=logging.INFO)
 logging.basicConfig(filename="accessc.log", format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
  
--- pulselock.py
+++ pulselock.py
@@ -1,17 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 import logging
+import subprocess
 
-logfile = 'accessc.log'
-#logging.basicConfig(filename="accessc.log", level=logging.INFO)
+
+logfile = "/home/accessc/Documents/accessc.log"
+#logging.basicConfig(filename="accessc.log", levell=logging.INFO)
 logging.basicConfig(filename="accessc.log", format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
  
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)
 #GPIO.output(12, GPIO.HIGH)
-#time.sleep(15)
+#print("Lock is open")
 GPIO.output(12, GPIO.LOW)
-logging.info('Lock via schedule')
 GPIO.cleanup()
+
+logging.info('Unlocked via pulse test')
+print("Lock is scheduled to unlock")
